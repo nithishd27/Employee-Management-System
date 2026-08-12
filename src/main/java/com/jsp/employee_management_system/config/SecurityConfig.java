c2d7267c9f7f7189e53d667df07b37e9e9095003
Nithish D <nithishd272005@gmail.com>
2026-08-12 18:02:18 +0530

    Added role wise authentication feature

--- src/main/java/com/jsp/employee_management_system/config/SecurityConfig.java
+++ src/main/java/com/jsp/employee_management_system/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jsp.employee_management_system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,7 +24,8 @@ public class SecurityConfig {
 		csrf(csrf->csrf.disable()).
 		authorizeHttpRequests(auth->auth.
 				requestMatchers("/users/register","/users/verify-otp").permitAll().
-				requestMatchers("/employees/**").authenticated().
+				requestMatchers(HttpMethod.GET,"/employees/**").hasAnyRole("ADMIN","USER").
+				requestMatchers("/employees/**").hasRole("ADMIN").
 				anyRequest().authenticated()).
 				httpBasic(Customizer.withDefaults());
 				
